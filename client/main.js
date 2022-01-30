c59d9e27d7f3b15792a9a0ac07d07a9621608ed1
Wesley Lo <wesleylo1995@gmail.com>
2022-01-29 21:54:31 -0600

    1 post and 3 get.

--- client/main.js
+++ client/main.js
@@ -1,68 +1,53 @@
 const compBtn = document.getElementById('complimentButton')
 const fortuneBtn = document.getElementById('fortuneButton')
-const submit = document.getElementById('submit')
-let inputField = document.querySelector('input')
 const body = document.body;
-const h1 = document.querySelector('h1')
-const ul = document.querySelector('ul')
-const colBtn = document.querySelector('#color')
-const name = document.querySelector('#name')
+const forText = document.querySelector('.fortune-text')
+const form = document.querySelector('form')
+const charBtn = document.querySelector('.character-button')
+const randomCharacter = document.querySelector('.character-text')
+const jokeBtn = document.querySelector('.joke')
+const joke = document.querySelector('.joke2')
 
 
-function compliment() {
-    axios.get("http://localhost:4000/api/compliment/").then(res => {
-        const data = res.data;
-        alert(data);
-    })
-}
-// random fortune
-function fortune() {
-    axios.get('http://localhost:4000/api/fortune/').then(res => {
-        const data = res.data;
-        const h2 = document.createElement('h2')
-        h2.textContent = data;
-        body.appendChild(h2)
-    })
-}
+const baseURL = 'http://localhost:4000/api'
 
-//new fortune
-function newFortune() {
-    axios.post('http://localhost:4000/api/new').then(() => {
-        let fortune = document.createElement('li')
-        let fortuneName = document.createElement('span')
-        fortuneName.textContent = inputField.value
-        fortune.appendChild(fortuneName)
-        ul.appendChild(fortune)
-        inputField.value = ''
-    })
-    .catch(err => console.log(err))
-}
 
-// random color
-function randomColor() {
-    axios.get('http://localhost:4000/api/color').then(res => {
-        const data = res.data;
-        const h2 = document.createElement('h2')
-        h2.textContent = data;
-        body.appendChild(h2)
-    })
-}
+const errCallback = err => console.log(err.res.data)
+const compliment = () => axios.get(`${baseURL}/compliment`).then(res => alert(res.data)).catch(errCallback)
+const getFortune = () => axios.get(`${baseURL}/fortune`).then(res => {forText.textContent = res.data}).catch(errCallback)
+const createCharacter = body => axios.post(`${baseURL}/newCharacter`,body).then(res => {alert(`character submitted`);console.log(res.data)}).catch(errCallback)
+const randomChar = () => axios.get(`${baseURL}/random`).then(res => {randomCharacter.textContent = res.data}).catch(errCallback)
 
-// random character
-function randomChar() {
-    axios.get('http://localhost:4000/api/name').then(res => {
-        const data = res.data;
-        const h2 = document.createElement('h2')
-        h2.textContent = data;
-        body.appendChild(h2)
-    })
+
+
+
+function submitHandler(e) {
+    e.preventDefault()
+
+    let name = document.querySelector('#name')
+    let age = document.querySelector('#age')
+
+    let charObj = {
+        name: name.value,
+        age: age.value
+    }
+
+    createCharacter(charObj)
+
+    name.value = ''
+    age.value = ''
 }
 
 
 
 
+
+fortuneBtn.addEventListener('click', getFortune)
 compBtn.addEventListener('click',compliment)
-fortuneBtn.addEventListener('click',fortune)
-submit.addEventListener('click',newFortune)
-colBtn.addEventListener('click',randomColor)
-name.addEventListener('click',randomChar)
+form.addEventListener('submit',submitHandler)
+charBtn.addEventListener('click',randomChar)
+jokeBtn.addEventListener('click',() => {
+    axios.get(`${baseURL}/jokes`).then(res => joke.textContent = res.data).catch(errCallback)
+})
+
+
--- server/index.js
+++ server/index.js
@@ -1,84 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+let { compliment,getFortune,createCharacter,randomChar,getJoke } = require('./controller.js')
 
 app.use(cors());
-
 app.use(express.json()); // When we want to be able to accept JSON.
 
 
-let fortunes = [
-  "The person who will not stand for something will fall for anything.",
-  "The best prediction of future is the past.",
-  "The strong person understands how to withstand substantial loss.",
-  "The harder you work, the luckier you get.",
-  "Listen not to vain words of empty tongue."
-]
-
-let colors = [
-  'red','blue','orange','pink','yellow'
-]
-
-let characters = [
-  'Joe','Jake','Bryan','Kelly','Joseph','Issac'
-]
-
-
 // To get a compliment 
-app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
-
-  // choose random compliment
-  let randomIndex = Math.floor(Math.random() * compliments.length);
-  let randomCompliment = compliments[randomIndex];
-
-  res.status(200).send(randomCompliment);
-  
-});
-
+app.get("/api/compliment", compliment)
 
 // To get a fortune
-app.get('/api/fortune', (req,res) => {
+app.get('/api/fortune', getFortune)
 
-  let randomIndex = Math.floor(Math.random() * fortunes.length);
-  let randomFortune = fortunes[randomIndex]
-
-  res.status(200).send(randomFortune)
-})
+// Create new character
+app.post('/api/newcharacter',createCharacter)
 
 
-// new fortune
-app.post('/api/new', (req,res) => {
-  let { fortuneInput } = req.body
-  let newFortune = {
-      input: fortuneInput,
-  }
-  fortunes.push(newFortune)
-  res.status(200).send(newFortune)
-})
+// Get a random character
+app.get('/api/random',randomChar)
 
-// color name
-app.get('/api/color', (req,res) => {
-  let randomIndex = Math.floor(Math.random() * colors.length);
-  let randomColor = colors[randomIndex]
-
-  res.status(200).send(randomColor)
-})
-
-// name
-app.get('/api/name', (req,res) => {
-  let randomIndex = Math.floor(Math.random() * characters.length)
-  let randomChar = characters[randomIndex]
-
-  res.status(200).send(randomChar)
-})
-
-
-
-
-
+// click for joke
+app.get('/api/jokes',getJoke)
+// server
 app.listen(4000, () => console.log("Server running on 4000"))
